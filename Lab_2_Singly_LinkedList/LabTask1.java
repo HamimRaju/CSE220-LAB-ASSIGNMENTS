14b3941ac5c3950dd9f24e25272b3fb188009241
Raju <rajumcsk@gmail.com>
2026-03-17 19:00:18 +0600

    Lab task 1 updated

---
+++ Lab_2_Singly_LinkedList/LabTask1.java
@@ -0,0 +1,31 @@
+public class LabTask1 {
+
+    public static Boolean assembleCongaLine(Node head){
+        Node current = head;
+        while (current.next != null) {
+            if((Integer)current.elem >  (Integer)current.next.elem){
+                return false;
+            }
+            current = current.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("==============Test Case 1=============");
+        Node congaLine = LinkedList.createList(new Integer[]{10,15,34,41,56,72});
+        System.out.print("Original Conga Line: ");
+        LinkedList.printLL(congaLine);
+        System.out.println("Expected output: true");
+        System.out.println( "Your output: "+assembleCongaLine(congaLine) ); //This should print True
+        System.out.println();
+        System.out.println("==============Test Case 2=============");
+        congaLine = LinkedList.createList(new Integer[]{10,15,44,41,56,72});
+        System.out.print("Original Conga Line: ");
+        LinkedList.printLL(congaLine);
+        System.out.println("Expected output: false");
+        System.out.println( "Your output: "+assembleCongaLine(congaLine) ); //This should print False
+        System.out.println();
+    }
+}
